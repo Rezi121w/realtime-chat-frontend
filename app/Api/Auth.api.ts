975c32ac1b39266be6c29e6ec20753bf67d33ba7
Rezi <reziabulashvili@gmail.com>
2024-06-16 00:40:35 +0400

    Update Auth.api.ts

--- app/Api/Auth.api.ts
+++ app/Api/Auth.api.ts
@@ -16,3 +16,54 @@ export const LoginApi = async (username: string, pass: string) => {
         return error;
     }
 };
+
+export const ChangeProfileApi = async (accessToken: string, imageLink: string) => {
+    try {
+        const response = await axios.post(`${BASEURL}/auth/me`, {
+            profileImage: imageLink,
+
+            headers: {
+                'Authorization': accessToken,
+            }
+        });
+        if(response.status === 201){
+            return response.data;
+        }
+    } catch(error) {
+        return error;
+    }
+};
+
+export const ChangePassApi = async (accessToken: string, lastPass: string, newPass: string) => {
+    try {
+        const response = await axios.post(`${BASEURL}/auth/me`, {
+            pass: lastPass,
+            newpass: newPass,
+
+            headers: {
+                'Authorization': accessToken,
+            }
+        });
+        if(response.status === 201){
+            return response.data;
+        }
+    } catch(error) {
+        return error;
+    }
+};
+
+export const GetUserApi = async (accessToken: string) => {
+    try {
+        const response = await axios.get(`${BASEURL}/auth/me`, {
+            headers: {
+                'Authorization': accessToken,
+            }
+        });
+        if(response.status === 200){
+            return response.data;
+        }
+    } catch(error) {
+        return error;
+    }
+};
+
