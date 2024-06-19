982e729eee27b576ac53769a083ad7e05828b548
Rezi <reziabulashvili@gmail.com>
2024-06-19 15:46:02 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ import LogIn from "@/app/components/logIn/logIn";
 
 export default function Home() {
     const router = useRouter();
+    if(typeof window !== "undefined") {
+        const accessToken = localStorage.getItem("user");
+        if(accessToken) {
+            router.push("/chats")
+        }
+    }
+
     return (
         <>
             <LogIn router={router}/>
